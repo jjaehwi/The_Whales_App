9ab68ed73a8215c3fe999ca804789ab3d9221869
HWI <dlwognl99@naver.com>
2022-11-02 19:36:22 +0900

    feat: 버튼 누르면 데이터 서버로 전송 기능 구현

--- src/screens/ControllerScreen.js
+++ src/screens/ControllerScreen.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Alert,
   AsyncStorage,
@@ -8,80 +8,133 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {Dimensions} from 'react-native';
-import {useDeviceOrientation} from '@react-native-community/hooks';
-import {useState} from 'react';
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Dimensions } from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { useState } from "react";
+import axios from "axios";
 
-const Width = Dimensions.get('window').width; //스크린 너비 초기화
-const Height = Dimensions.get('window').height; //스크린 높이 초기화
+const Width = Dimensions.get("window").width; //스크린 너비 초기화
+const Height = Dimensions.get("window").height; //스크린 높이 초기화
+const URL = "http://172.20.10.2/control/direction";
 
-function ControllerScreen({navigation}) {
-  const {landscape} = useDeviceOrientation();
+function ControllerScreen({ navigation }) {
+  const { landscape } = useDeviceOrientation();
 
-  const [text, setText] = useState('없음');
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("없음");
+  const [speed, setSpeed] = useState(0);
   const [heading, setHeading] = useState(0);
 
+  const postData = () => {};
+
   const LeftPress = () => {
-    setText('좌');
-    setHeading(heading - 15);
-    console.log('좌');
+    setText("좌");
+    setHeading(heading - 1);
+    console.log("좌");
+    axios
+      .post("http://172.20.10.9:8080/control/direction", {
+        a: speed,
+        d: heading,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
   const RightPress = () => {
-    setText('우');
-    setHeading(heading + 15);
-    console.log('우');
+    setText("우");
+    setHeading(heading + 1);
+    console.log("우");
+    axios
+      .post("http://172.20.10.9:8080/control/direction", {
+        a: speed,
+        d: heading,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
   const AbovePress = () => {
-    setText('상');
-    setCount(count + 10);
-    console.log('상');
+    setText("상");
+    setSpeed(speed + 1);
+    console.log("상");
+    axios
+      .post("http://172.20.10.9:8080/control/direction", {
+        a: speed,
+        d: heading,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
   const BelowPress = () => {
-    setText('하');
-    setCount(count - 10);
-    console.log('하');
+    setText("하");
+    setSpeed(speed - 1);
+    console.log("하");
+    axios
+      .post("http://172.20.10.9:8080/control/direction", {
+        a: speed,
+        d: heading,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: 'flex-end',
-        alignContent: 'center',
-        backgroundColor: '#475053',
-      }}>
+        justifyContent: "flex-end",
+        alignContent: "center",
+        backgroundColor: "#475053",
+      }}
+    >
       <View
         style={{
           flex: 1,
-          flexDirection: 'column',
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          flexDirection: "column",
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           left: landscape ? 0 : 0,
           top: landscape ? 0 : 0,
-        }}>
+        }}
+      >
         <Text
           style={{
             fontSize: 25,
-            color: 'white',
-          }}>
+            color: "white",
+          }}
+        >
           입력되는 값: {text}
         </Text>
         <Text
           style={{
             fontSize: 25,
-            color: 'white',
-          }}>
-          속도 : {count}
+            color: "white",
+          }}
+        >
+          속도 : {speed}
         </Text>
         <Text
           style={{
             fontSize: 25,
-            color: 'white',
-          }}>
+            color: "white",
+          }}
+        >
           타각 : {heading}
         </Text>
       </View>
@@ -89,11 +142,11 @@ function ControllerScreen({navigation}) {
       <TouchableOpacity
         style={{
           flex: 1,
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           right: 15,
           top: 15,
           height: 50,
@@ -102,25 +155,26 @@ function ControllerScreen({navigation}) {
         title="나가기"
         onPress={() => {
           Alert.alert(
-            '나가기',
-            '홈으로 가시겠습니까?',
+            "나가기",
+            "홈으로 가시겠습니까?",
             [
               {
-                text: '아니오',
+                text: "아니오",
                 onPress: () => {
                   return null;
                 },
               },
               {
-                text: '예',
+                text: "예",
                 onPress: () => {
-                  navigation.replace('HomeScreen');
+                  navigation.replace("HomeScreen");
                 },
               },
             ],
-            {cancelable: false},
+            { cancelable: false }
           );
-        }}>
+        }}
+      >
         <Text>나가기</Text>
       </TouchableOpacity>
       {/* 로그아웃 버튼 */}
@@ -128,11 +182,11 @@ function ControllerScreen({navigation}) {
       <TouchableOpacity
         style={{
           flex: 1,
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           right: 15,
           top: 75,
           height: 50,
@@ -141,44 +195,46 @@ function ControllerScreen({navigation}) {
         title="로그아웃"
         onPress={() => {
           Alert.alert(
-            '로그아웃',
-            '로그아웃 하시겠습니까?',
+            "로그아웃",
+            "로그아웃 하시겠습니까?",
             [
               {
-                text: '아니오',
+                text: "아니오",
                 onPress: () => {
                   return null;
                 },
               },
               {
-                text: '예',
+                text: "예",
                 onPress: () => {
                   AsyncStorage.clear();
-                  navigation.replace('LoginScreen');
+                  navigation.replace("LoginScreen");
                 },
               },
             ],
-            {cancelable: false},
+            { cancelable: false }
           );
-        }}>
+        }}
+      >
         <Text>로그아웃</Text>
       </TouchableOpacity>
 
       {/* 왼쪽 */}
       <TouchableOpacity
         style={{
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           bottom: 20,
           left: 30,
           height: 100,
           width: 100,
         }}
         title="왼쪽"
-        onPress={LeftPress}>
+        onPress={LeftPress}
+      >
         <Text>왼쪽</Text>
         <Icon name="arrow-left" size={24} color="black" />
       </TouchableOpacity>
@@ -186,18 +242,19 @@ function ControllerScreen({navigation}) {
       {/* 오른쪽 */}
       <TouchableOpacity
         style={{
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           bottom: 20,
           left: 275,
           height: 100,
           width: 100,
         }}
         title="오른쪽"
-        onPress={RightPress}>
+        onPress={RightPress}
+      >
         <Text>오른쪽</Text>
         <Icon name="arrow-right" size={24} color="black" />
       </TouchableOpacity>
@@ -205,18 +262,19 @@ function ControllerScreen({navigation}) {
       {/* 위 */}
       <TouchableOpacity
         style={{
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           bottom: 140,
           left: 150,
           height: 100,
           width: 100,
         }}
         title="위"
-        onPress={AbovePress}>
+        onPress={AbovePress}
+      >
         <Text>위</Text>
         <Icon name="arrow-up" size={24} color="black" />
       </TouchableOpacity>
@@ -224,18 +282,19 @@ function ControllerScreen({navigation}) {
       {/* 아래 */}
       <TouchableOpacity
         style={{
-          position: 'absolute',
-          alignItems: 'center',
-          justifyContent: 'center',
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
           borderRadius: 10,
-          backgroundColor: '#2e94b9',
+          backgroundColor: "#2e94b9",
           bottom: 20,
           left: 150,
           height: 100,
           width: 100,
         }}
         title="아래"
-        onPress={BelowPress}>
+        onPress={BelowPress}
+      >
         <Text>아래</Text>
         <Icon name="arrow-down" size={24} color="black" />
       </TouchableOpacity>
